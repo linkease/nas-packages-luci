c232131af002f62ad4ab8e4c63fda79d25ec6df0
janson <janson@gmail.com>
2024-08-06 18:32:55 +0800

    fix istore translate

--- luci/luci-app-istorex/htdocs/luci-static/istorex/chunk.2007e0c6.js
+++
@@ -1,1 +0,0 @@
-import{_ as r}from"./index.js?v=7aac662c";const e={};function n(_,c){return null}var a=r(e,[["render",n]]);export{a as default};
---
+++ luci/luci-app-istorex/htdocs/luci-static/istorex/chunk.e62dce9c.js
@@ -0,0 +1,1 @@
+import{_ as r}from"./index.js?v=068cfe92";const e={};function n(_,c){return null}var a=r(e,[["render",n]]);export{a as default};
